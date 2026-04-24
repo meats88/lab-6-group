fd9b107cfec535d0f448d928c96d8f2a8e9405e6
meats88 <pcx883mi@gmail.com>
2026-04-24 15:00:24 -0700

    input validation

--- input.hpp
+++ input.hpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void getInput(int &carbs, int &protein, int &fat, int &alcohol) {
+    do {
     cout << "Enter carbs (g): ";
     cin >> carbs;
     cout << "Enter protein (g): ";
@@ -11,4 +12,8 @@ void getInput(int &carbs, int &protein, int &fat, int &alcohol) {
     cin >> fat;
     cout << "Enter alcohol (g): ";
     cin >> alcohol;
+    if (carbs < 0 || protein < 0 || fat < 0 || alcohol < 0) {
+        cout << "no negative macros." << endl;
+    }
+    } while (carbs < 0 || protein < 0 || fat < 0 || alcohol < 0);
 }
